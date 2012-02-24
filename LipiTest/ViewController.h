db6755af4aa6b51641c2b6e54e4403dc8d82b49c
Eugene Dorfman <eugene@Eugene-Dorfmans-MacBook-Pro.local>
2012-02-24 13:52:16 +0200

    Initial Commit

---
+++ LipiTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LipiTest
+//
+//  Created by Eugene Dorfman on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
